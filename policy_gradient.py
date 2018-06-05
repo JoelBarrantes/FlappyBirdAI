be1a43ce1a77e24d9e49e33893babd4c04502525
Joel Barrantes <joel.bg95@outlook.com>
2018-06-04 20:57:59 -0600

    Release

--- policy_gradient.py
+++ policy_gradient.py
@@ -1,66 +1,142 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Bernoulli
+from torch.autograd import Variable
+from itertools import count
+import matplotlib.pyplot as plt
 import numpy as np
-from environment import Envir
-import torch.optim as optim
-
-batch_size = 10 # every how many episodes to do a param update?
+import gym
+import pdb
 
 
+class PolicyNet(nn.Module):
+    def __init__(self):
+        super(PolicyNet, self).__init__()
 
-D = 100 * 100 # input dimensionality: 80x80 grid
+        self.fc1 = nn.Linear(4, 24)
+        self.fc2 = nn.Linear(24, 36)
+        self.fc3 = nn.Linear(36, 1)  # Prob of Left
 
-criterion = nn.CrossEntropyLoss()
-
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.sigmoid(self.fc3(x))
+        return x
 
 
 def main():
 
-# model initialization
+    # Plot duration curve:
+    # From http://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+    episode_durations = []
+    def plot_durations():
+        plt.figure(2)
+        plt.clf()
+        durations_t = torch.FloatTensor(episode_durations)
+        plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Duration')
+        plt.plot(durations_t.numpy())
+        # Take 100 episode averages and plot them too
+        if len(durations_t) >= 100:
+            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            plt.plot(means.numpy())
 
-    # hyperparameters
+        plt.pause(0.001)  # pause a bit so that plots are updated
 
-    #CONV_NET PARAMETERS
+    # Parameters
+    num_episode = 5000
+    batch_size = 5
+    learning_rate = 0.01
+    gamma = 0.99
 
+    env = gym.make('CartPole-v0')
+    policy_net = PolicyNet()
+    optimizer = torch.optim.RMSprop(policy_net.parameters(), lr=learning_rate)
 
-
-      # Batch History
-
-    env = Envir('FlappyBird-v0',False)
-
-    optimizer = optim.RMSprop(env.policy.parameters(), lr=learning_rate, weight_decay=decay_rate)
-
-    for e in range(0, max_episodes):
-
-        env.step()
-
-
-        discounted_epr = discount_rewards(np.vstack(env.rewards))
-            # standardize the rewards to be unit normal (helps control the gradient estimator variance)
-        discounted_epr -= np.mean(discounted_epr)
-        discounted_epr /= np.std(discounted_epr)
+    # Batch History
+    state_pool = []
+    action_pool = []
+    reward_pool = []
+    steps = 0
 
 
-        policy_loss = []
-        for log_prob, reward in zip(env.log_probs, discounted_epr):
-            policy_loss.append(-log_prob * reward)
+    for e in range(num_episode):
 
-        optimizer.zero_grad()
+        state = env.reset()
+        state = torch.from_numpy(state).float()
+        state = Variable(state)
+        env.render(mode='rgb_array')
 
-        policy_loss = torch.cat(policy_loss).sum()
-        policy_loss.backward()
-        optimizer.step()
+        for t in count():
 
-        env.reset()
+            probs = policy_net(state)
+            m = Bernoulli(probs)
+            action = m.sample()
 
-def discount_rewards(r):
+            action = action.data.numpy().astype(int)[0]
+            next_state, reward, done, _ = env.step(action)
+            env.render(mode='rgb_array')
 
-    discounted_r = np.zeros_like(r)
-    running_add = 0
-    for t in reversed(range(0, r.size)):
-        if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
-        running_add = running_add * gamma + r[t]
-        discounted_r[t] = running_add
-    return discounted_r
+            # To mark boundarys between episodes
+            if done:
+                reward = 0
 
-main()
+            state_pool.append(state)
+            action_pool.append(float(action))
+            reward_pool.append(reward)
+
+            state = next_state
+            state = torch.from_numpy(state).float()
+            state = Variable(state)
+
+            steps += 1
+
+            if done:
+                episode_durations.append(t + 1)
+                plot_durations()
+                break
+
+        # Update policy
+        if e > 0 and e % batch_size == 0:
+
+            # Discount reward
+            running_add = 0
+            for i in reversed(range(steps)):
+                if reward_pool[i] == 0:
+                    running_add = 0
+                else:
+                    running_add = running_add * gamma + reward_pool[i]
+                    reward_pool[i] = running_add
+
+            # Normalize reward
+            reward_mean = np.mean(reward_pool)
+            reward_std = np.std(reward_pool)
+            for i in range(steps):
+                reward_pool[i] = (reward_pool[i] - reward_mean) / reward_std
+
+            # Gradient Desent
+            optimizer.zero_grad()
+
+            for i in range(steps):
+                state = state_pool[i]
+                action = Variable(torch.FloatTensor([action_pool[i]]))
+                reward = reward_pool[i]
+
+                probs = policy_net(state)
+                m = Bernoulli(probs)
+                loss = -m.log_prob(action) * reward  # Negtive score function x reward
+                loss.backward()
+
+            optimizer.step()
+
+            state_pool = []
+            action_pool = []
+            reward_pool = []
+            steps = 0
+
+
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -5,8 +5,6 @@ import time
 import cv2
 import gym_ple
 
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,7 +14,6 @@ from torch.distributions import Categorical
 from itertools import count
 from scipy import misc
 from skimage import measure
-from policy import Policy
 
 
 parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
@@ -36,6 +33,7 @@ env.seed(args.seed)
 torch.manual_seed(args.seed)
 
 learning_rate = 1e-4
+#learning_rate = 0.0085
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 resume = False
@@ -46,51 +44,83 @@ render = True
 max_episodes = 1000
 date_time = time.strftime("%Y.%m.%d-%H.%M.%S")
 model_path = 'Model-'+date_time+'.pt'
-model_load = "Model-2018.05.28-19.33.14.pt"
+model_load = "Model-2018.05.30-01.13.48.pt"
 
 
-class Policy(nn.Module):
+class Policy1(nn.Module):
 
     # cnn_layers is a list of lists that represent a layer. i.e. [[input_ch, out_ch, kernel_size, stride, padding], [.....]] is a list with two convolutional layers.
     def __init__(self):
-        super(Policy, self).__init__()
-        self.conv1 = nn.Conv2d(4,32,3,stride=2,padding=0)
-        self.conv2 = nn.Conv2d(32,32,3,stride=2,padding=0)
-        self.conv3 = nn.Conv2d(32,32,3,stride=2,padding=0)
-        self.conv4 = nn.Conv2d(32,32,3,stride=2,padding=0)
+        super(Policy1, self).__init__()
+        self.conv1 = nn.Conv2d(4,10,3,stride=1,padding=0)
+        self.conv2 = nn.Conv2d(10,5,3,stride=1,padding=0)
+        #self.conv3 = nn.Conv2d(10,5,3,stride=1,padding=0)
+  #      self.conv4 = nn.Conv2d(32,32,3,stride=1,padding=0)
 
-        self.fc1= nn.Linear(800, 256)
-        self.fc2= nn.Linear(256, 2)
+        self.fc1= nn.Linear(845, 64)
+       # self.fc2= nn.Linear(256, 64)
+        self.fc2= nn.Linear(64, 2)
 
 
         self.saved_log_probs = []
         self.rewards = []
         self.states = []
-        self.running_reward = 10
+        self.running_reward = 0
 
     def forward(self, x):
 
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-
-        x = x.view(-1, 800)
+        x = F.relu(F.max_pool2d(self.conv1(x),2))
+        x = F.relu(F.max_pool2d(self.conv2(x),2))
+        #x = F.relu((self.conv3(x)))
+     #   x = F.relu(F.max_pool2d(self.conv4(x),2))
+        x = x.view(-1, 845)
 
         x = F.relu(self.fc1(x))
-        #x = F.relu(self.fc2(x))
+        #x = F.dropout(x, training=self.training)
+        # x = F.relu(self.fc2(x))
         x = F.softmax(self.fc2(x), dim=1)
         return x
 
+
+class Policy(torch.nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+
+        self.conv = torch.nn.Sequential()
+        self.conv.add_module("conv_1", torch.nn.Conv2d(4, 32, kernel_size=3))
+        self.conv.add_module("maxpool_1", torch.nn.MaxPool2d(kernel_size=2))
+        self.conv.add_module("relu_1", torch.nn.ReLU())
+        self.conv.add_module("conv_2", torch.nn.Conv2d(32, 32, kernel_size=3))
+        self.conv.add_module("dropout_2", torch.nn.Dropout())
+        self.conv.add_module("maxpool_2", torch.nn.MaxPool2d(kernel_size=2))
+        self.conv.add_module("relu_2", torch.nn.ReLU())
+
+        self.fc = torch.nn.Sequential()
+        self.fc.add_module("fc1", torch.nn.Linear(320*10, 50))
+        self.fc.add_module("relu_3", torch.nn.ReLU())
+        self.fc.add_module("dropout_3", torch.nn.Dropout())
+        self.fc.add_module("fc2", torch.nn.Linear(50, 2))
+        self.fc.add_module("softmax_1", torch.nn.Softmax())
+
+        self.saved_log_probs = []
+        self.rewards = []
+        self.states = []
+        self.running_reward = 0
+
+    def forward(self, x):
+        x = self.conv.forward(x)
+        x = x.view(-1, 320*10)
+        return self.fc.forward(x)
 
 
 
 if resume:
     policy = torch.load(model_load)
 else:
-    policy = Policy()
+    policy = Policy1()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adagrad(policy.parameters(), lr=learning_rate)
+optimizer = optim.RMSprop(policy.parameters(), lr=learning_rate)
 eps = np.finfo(np.float32).eps.item()
 
 def select_action(state):
@@ -99,8 +129,8 @@ def select_action(state):
     probs = policy(state)#The output of the model
     m = Categorical(probs)
     action = m.sample() #The "Fake label"
-    #policy.saved_log_probs.append(m.log_prob(action))
-    policy.saved_log_probs.append(torch.tensor([criterion(probs, action)],requires_grad=True))#Cross entropy loss
+    policy.saved_log_probs.append(-m.log_prob(action))
+    #policy.saved_log_probs.append(criterion(probs, action))#Cross entropy loss
     return action
 
 def finish_episode():
@@ -113,24 +143,27 @@ def finish_episode():
     rewards = torch.tensor(rewards)
     rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
 
+    rewards = policy.rewards
+
     optimizer.zero_grad()
-    for reward, state in zip(rewards, policy.states):
+    for loss, reward, state in zip(policy.saved_log_probs,rewards, policy.states):
 
-        state = torch.from_numpy(state).float()
-        state.requires_grad=True
-        probs = policy(state)#The output of the model
-        m = Categorical(probs)
-        action = m.sample() #The "Fake label"
-        policy_loss = criterion(probs, action) * reward
-        policy_loss.backward()
+    #    state = torch.from_numpy(state).float()
+    #    state.requires_grad=True
+    #    probs = policy(state)#The output of the model
+    #    m = Categorical(probs)
+    #    action = m.sample() #The "Fake label"
+    #    policy_loss = criterion(probs, action) * reward
+    #    policy_loss.backward()
+        loss_t = loss * reward
+        policy_loss.append(loss_t)
 
-    #   policy_loss.append(loss * reward)
 
-
-    #policy_loss = torch.cat(policy_loss).sum()
-    #policy_loss.backward()
+    policy_loss = torch.cat(policy_loss).sum()
+    print(policy_loss)
+    policy_loss.backward()
+    #optimizer.step()
     optimizer.step()
-
     del policy.states[:]
     del policy.rewards[:]
     del policy.saved_log_probs[:]
@@ -138,7 +171,7 @@ def finish_episode():
 
 def _preprocess_observation(observation):
 
-  #  observation = observation[0:404, :]
+    observation = observation[0:404, :]
     red, green, blue = observation[:,:,0], observation[:,:,1], observation[:,:,2]
     mask =((red == 83) & (green == 56 ) & (blue == 70 )) | (red == 73 ) & (green == 36 ) & (blue == 85) | \
           (red == 84) & (green == 56 ) & (blue == 71) | (red == 80) & (green == 67) &  ( blue == 97)
@@ -151,7 +184,7 @@ def _preprocess_observation(observation):
     image[image == 1] = 255
     image[image < 255 ] = 0
 
-    return misc.imresize((255 - image.astype(np.uint8)), (47, 47))
+    return misc.imresize((255 - image.astype(np.uint8)), (60, 60))
 
 def main():
     threshold = 0
@@ -173,6 +206,12 @@ def main():
                 action = select_action(state)
 
             state, reward, done, _ = env.step(action)
+
+            if reward < 0:
+                reward = -5
+            if reward > 1:
+                reward = 1
+
             state = _preprocess_observation(state)
             state = state.reshape(1, 1, state.shape[0], state.shape[1])
 
@@ -183,13 +222,16 @@ def main():
 
                 s_t3 = np.append(s_t2, states[len(states)-3], axis=1)
                 state = s_t3
-                policy.rewards.append(reward)
+
                 policy.states.append(state)
+                policy.rewards.append(reward)
+
             if t>3:
                 s_t1 = np.append(state, states[len(states)-1][:,:3,:,:], axis=1)
                 state = s_t1
                 policy.rewards.append(reward)
                 policy.states.append(state)
+
             states.append(state.copy())
 
 
@@ -199,34 +241,41 @@ def main():
                 env.render()
 
             if t>=3:
+                episode_length +=1
                 if reward<0:
-                    reward=-1
+                    old_threshold = threshold
+                    threshold = episode_length - 20
+                    if threshold<old_threshold:
+                        threshold = old_threshold
                     policy.rewards[threshold:]=[-1]*len(policy.rewards[threshold:])
                     threshold = episode_length
 
 
                 if reward>0:
-                    reward=5
+
+
+                    #threshold = episode_length - 20
+                    # if threshold<0:
+                    #    threshold = 0
                     policy.rewards[threshold:]= [1]*len(policy.rewards[threshold:])
                     threshold = episode_length
 
 
             if done:
                 break
-            episode_length +=1
 
 
         if i_episode>0 and  i_episode%batch_size==0:
             threshold = 0
-            running_reward = running_reward * gamma + t * 0.01
+            running_reward = running_reward + episode_length
             policy.running_reward = running_reward
             finish_episode()
             if i_episode % args.log_interval == 0:
 
                 torch.save(policy, model_path)
 
-                print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(
-                    i_episode, t, running_reward))
+                print('Episode {}\tEpisode length: {:5d}\tAverage length: {:.2f}'.format(
+                    i_episode, episode_length, running_reward/i_episode+1))
 
 
 
